e26b2f3b16c7dea88bddaf28fa2ff12f24631fe2
victor <vgonzalez@lifesize.com>
2021-03-19 10:34:22 -0600

    Refactored code and created config files.

--- data_recollect/blueprints/cryptos.py
+++ data_recollect/blueprints/cryptos.py
@@ -3,15 +3,12 @@ from flask import Blueprint, jsonify
 
 from data_recollect.config import config
 
-
 cryptos = Blueprint('cryptos', __name__)
-
-base_url = 'https://financialmodelingprep.com/api/v3/historical-chart/1min'
 
 
 @cryptos.route('/cryptos/<string:crypto_ticker>')
 def get_min_data(crypto_ticker):
-    endpoint = f'{base_url}/{crypto_ticker}?apikey={config.API_TOKEN}'
+    endpoint = f'{config.BASE_URL}/{crypto_ticker}?apikey={config.API_TOKEN}'
     response = requests.get(endpoint)
     payload = response.json()
     print(payload)
--- data_recollect/config/config.py
+++ data_recollect/config/config.py
@@ -1,3 +1,4 @@
 from data_recollect.utils.utils import get_env
 
 API_TOKEN = get_env('API_TOKEN', 'fdc87acc05b63069ece12db7d63a9541')
+BASE_URL = get_env('BASE_URL', 'https://financialmodelingprep.com/api/v3/historical-chart/1min')
